a7b913e7017dbd66af2a8fc4b9dd8011a000c01f
amargh2 <margherioa@gmail.com>
2022-02-22 20:26:58 -0600

    Defined buttons, clear and input functions, and got the display working. Working on passing input to the operate function.

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
+display = document.querySelector(".display").textContent
+
 function add(a, b) {
     return (a + b);
 }
@@ -14,7 +16,7 @@ function multiply(a, b) {
     return (a * b)
 }
 
-function operate(operator, a, b) {
+function operate(a, operator, b) {
     if (operator == '*') {
         return multiply(a, b)
     }
@@ -28,3 +30,93 @@ function operate(operator, a, b) {
         return divide (a, b)
     }
 }
+
+function input(a) {
+    display = document.querySelector(".display");
+    display.textContent += a;
+}
+
+function clearInput() {
+    display = document.querySelector(".display");
+    display.textContent = '';
+}
+
+clearButton = document.querySelector('.clear')
+clearButton.addEventListener('click', () => {
+    clearInput()
+})
+
+oneButton = document.querySelector(".one")
+oneButton.addEventListener('click', () => {
+    input(1)
+})
+
+twoButton = document.querySelector(".two")
+twoButton.addEventListener('click', () => {
+    input(2)
+})
+
+threeButton = document.querySelector(".three")
+threeButton.addEventListener('click', () => {
+    input(3)
+})
+
+fourButton = document.querySelector(".four")
+fourButton.addEventListener('click', () => {
+    input(4)
+})
+
+fiveButton = document.querySelector(".five")
+fiveButton.addEventListener('click', () => {
+    input(5)
+})
+
+sixButton = document.querySelector(".six")
+sixButton.addEventListener('click', () => {
+    input(6)
+})
+
+sevenButton = document.querySelector(".seven")
+sevenButton.addEventListener('click', () => {
+    input(7)
+})
+
+eightButton = document.querySelector(".eight")
+eightButton.addEventListener('click', () => {
+    input(8)
+})
+
+nineButton = document.querySelector(".nine")
+nineButton.addEventListener('click', () => {
+    input(9)
+})
+
+zeroButton = document.querySelector(".last-button")
+zeroButton.addEventListener('click', () => {
+    input(0)
+})
+
+plusButton = document.querySelector(".add")
+plusButton.addEventListener('click', () => {
+    input("+")
+})
+
+divideButton = document.querySelector(".divide")
+divideButton.addEventListener('click', () => {
+    input("/")
+})
+
+multiplyButton = document.querySelector(".divide")
+zeroButton.addEventListener('click', () => {
+    input(0)
+})
+
+subtractButton = document.querySelector(".divide")
+zeroButton.addEventListener('click', () => {
+    input(0)
+})
+
+equalButton = document.querySelector(".equals")
+equalButton.addEventListener('click', () => {
+    operate(display)
+})
