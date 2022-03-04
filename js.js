22e629491992c8d454303cc20382637311fd1c75
amargh2 <margherioa@gmail.com>
2022-03-04 13:30:19 -0600

    got the basics working. does basic operations. easy to break, and looks not so great, but that can be worked on later.

--- js.js
+++ js.js
@@ -1,7 +1,9 @@
 display = document.querySelector(".display").textContent
+display = ''
+clearInput()
 
 function add(a, b) {
-    return (a + b);
+    return (parseInt(a) + parseInt(b));
 }
 
 function subtract(a, b) {
@@ -17,6 +19,7 @@ function multiply(a, b) {
 }
 
 function operate(a, operator, b) {
+
     if (operator == '*') {
         return multiply(a, b)
     }
@@ -38,7 +41,7 @@ function input(a) {
 
 function clearInput() {
     display = document.querySelector(".display");
-    display.textContent = '';
+    display.textContent = ""
 }
 
 clearButton = document.querySelector('.clear')
@@ -98,25 +101,36 @@ zeroButton.addEventListener('click', () => {
 
 plusButton = document.querySelector(".add")
 plusButton.addEventListener('click', () => {
-    input("+")
+    input(" + ")
 })
 
 divideButton = document.querySelector(".divide")
 divideButton.addEventListener('click', () => {
-    input("/")
+    input(" / ")
 })
 
-multiplyButton = document.querySelector(".divide")
-zeroButton.addEventListener('click', () => {
-    input(0)
+multiplyButton = document.querySelector(".multiply")
+multiplyButton.addEventListener('click', () => {
+    input(" * ")
 })
 
-subtractButton = document.querySelector(".divide")
-zeroButton.addEventListener('click', () => {
-    input(0)
+subtractButton = document.querySelector(".subtract")
+subtractButton.addEventListener('click', () => {
+    input(" - ")
 })
 
 equalButton = document.querySelector(".equals")
 equalButton.addEventListener('click', () => {
-    operate(display)
+    display = document.querySelector(".display").textContent
+    console.log(display)
+    display = display.split(' ')
+    console.log(display)
+    result = operate(display[0], display[1], display[2])
+    console.log(result)
+    document.querySelector(".display").textContent = result
 })
+
+function parseInput() {
+    
+    return display
+}
